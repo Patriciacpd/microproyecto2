153cfbffec6e5476ddef8fa9cf6e15c15e858830
fabiolaguia <fabiolaguiaf@gmail.com>
2021-03-05 14:17:15 -0400

    api servicio

---
+++ src/app/apipelicula.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class APIpeliculaService {
+
+  constructor(protected http:HttpClient) { }
+
+  apiKey= "?api_key=840fda7ee6e97d936d368f8a20db8877"
+  apiURL= "https://api.themoviedb.org/3/"
+
+  getMovieDetail(id:string){ 
+    return this.http.get(this.apiURL+"movie/"+id+this.apiKey);
+  }
+}
