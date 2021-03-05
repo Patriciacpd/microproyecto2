4a8a368e8f73652a73e4c6b2f2b4206a20e18aa7
fabiolaguia <fabiolaguiaf@gmail.com>
2021-03-05 14:05:36 -0400

    servicio api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InfoPeliculaComponent } from './InfoPelicula/info-pelicula/info-pelicula.component';
+import { ListaReservasComponent } from './ListaReservas/lista-reservas/lista-reservas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoPeliculaComponent
+    InfoPeliculaComponent,
+    ListaReservasComponent
   ],
   imports: [
     BrowserModule
