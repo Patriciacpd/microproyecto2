70aece02fd809bfc222d406de263b1e36cde608d
Patricia <patriciacpd9@gmail.com>
2021-03-05 14:11:14 -0400

    reserva,registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/login/login.component';
+import { CrearReservaComponent } from './crearReserva/crear-reserva/crear-reserva.component';
+import { RegistroComponent } from './Registro/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CrearReservaComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
